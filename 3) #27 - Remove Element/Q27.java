43a9242dbec341e5bd323129d0f4c061fff205d6
Anshul2202 <anshul220206@gmail.com>
2026-05-23 03:01:55 +0530

    Solved question number 27 on leetcode

---
+++ 3) #27 - Remove Element/Q27.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int a = 0;
+        int k = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] == val){
+                continue;
+            } else{
+                nums[a] = nums[i];
+                k++;
+                a++;
+            }
+        }
+        return k;
+    }
+}
