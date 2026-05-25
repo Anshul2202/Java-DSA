3d79ff73cddefe0599e327a8cbc912bf2731d45c
Anshul2202 <anshul220206@gmail.com>
2026-05-25 20:42:59 +0530

    Solved question 125

---
+++ 5) #125 - Valid Palindrome/Q125.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+        boolean result = true;
+
+        while(i < j){
+
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+
+            while(j > i && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+
+            if(s.charAt(i) == s.charAt(j)){
+                result = true;
+            } else{
+                result = false;
+                return result;
+            }
+
+            i++;
+            j--;
+        }
+
+        return result;
+    }
+}
