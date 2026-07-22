48995a750d3bbdabf741aa07d57ad3116ad05e92
Anshul2202 <anshul220206@gmail.com>
2026-07-22 23:30:07 +0530

    Learned more about Patterns

--- 0001) Patterns/Pattern.java
+++ 0001) Patterns/Pattern.java
@@ -350,6 +350,25 @@ class Pattern{
             for(int j = 0; j < row; j++){
                 System.out.print("*");
             }
+           
+            System.out.println();
+        }
+
+        // ------------------------------------------------
+
+        // Pattern 21
+        for(int i = 0; i < 4; i++){
+
+            for(int j = 0; j < 4; j++){
+
+                if(i == 0 || i == 4 - 1 || j == 0 || j == 4 - 1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
             System.out.println();
         }
     }
